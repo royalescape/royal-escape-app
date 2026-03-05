e9e98417e543b1ec96e86a2df1f6724936de5f14
rohan <rohan.doshi@bazaarvoice.com>
2026-03-05 16:04:37 +0530

    updating public image assets

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -309,9 +309,11 @@ export default function RoyalEscapeHome() {
             {/* Image Carousel */}
             {currentView === 'home' && (
                 <ImageCarousel images={[
+                   "/1.jpg",
                    "/2.jpg",
                    "/3.jpg",
                    "/4.jpg",
+                   "/5.jpg",
                    "/6.jpg",
                    "/7.jpg",
                    "/8.jpg",
