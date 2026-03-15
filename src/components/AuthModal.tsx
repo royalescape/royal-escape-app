ebdcf83a576bd88a4751046402e958ff877007ef
rohan <rohan.doshi@bazaarvoice.com>
2026-03-15 13:20:28 +0530

    making pin only numeric

--- src/components/AuthModal.tsx
+++ src/components/AuthModal.tsx
@@ -191,6 +191,7 @@ const AuthModal: React.FC<AuthModalProps> = ({ isOpen, onClose, onAuthSuccess })
                                     <Lock className="absolute left-3 top-1/2 transform -translate-y-1/2 w-5 h-5 text-gray-500" />
                                     <input
                                         type="password"
+                                        inputMode="numeric"
                                         value={pin}
                                         onChange={(e) => setPin(e.target.value.replace(/\D/g, ''))}
                                         placeholder="••••"
